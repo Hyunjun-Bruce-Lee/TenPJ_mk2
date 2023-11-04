16ae6a6f41d6b08c8687c8b804e12fefb8f4d020
Hyuunjun-Bruce-Lee <hyunjun.bruce.lee@gmail.com>
2023-11-04 20:21:09 +0900

    :construction:

--- image_stampping_test.py
+++ image_stampping_test.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
@@ -14,39 +15,174 @@ drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 # 이미지 읽기
 image = cv2.imread("/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/test_img/p179506928231510_590.jpg")
+king_stamp = cv2.imread('/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/king.jpeg')
 
 # 얼굴 검출
+# 21 : left idx
+# 251 : right idx
 results = face_mesh.process(image) 
 
+person_idx = 2
 
-x = results.multi_face_landmarks[0].landmark[1].x
-y = results.multi_face_landmarks[0].landmark[1].y
+middle_x = results.multi_face_landmarks[person_idx].landmark[151].x
+middle_y = results.multi_face_landmarks[person_idx].landmark[151].y
 
-o_y, o_x, ch = np.array(image).shape
+left_x = results.multi_face_landmarks[person_idx].landmark[162].x
+left_y = results.multi_face_landmarks[person_idx].landmark[162].y
+right_x = results.multi_face_landmarks[person_idx].landmark[389].x
+right_y = results.multi_face_landmarks[person_idx].landmark[389].y
 
+bottom_x = results.multi_face_landmarks[person_idx].landmark[199].x
+bottom_y = results.multi_face_landmarks[person_idx].landmark[199].y
 
-n_x = x * o_x
-n_y = y * o_y
+o_y, o_x, ch = image.shape
 
-n_x_i = int(n_x)
-n_y_i = int(n_y)
+middle_x_act = int(middle_x * o_x)
+middle_y_act = int(middle_y * o_y)
+left_x_act = math.floor(left_x * o_x)
+right_x_act = math.ceil(right_x * o_x)
+left_y_act = int(left_y * o_y)
+right_y_act = int(right_y * o_y)
+bottom_x_act = int(bottom_x * o_x)
+bottom_y_act = int(bottom_y * o_y)
+
+common_y = left_y_act if left_y_act >= right_y_act else right_y_act
 
 drawing_image = image.copy()
- 
-cv2.circle(drawing_image, (n_x_i, n_y_i), 10, (0, 0, 255), 3) 
+
+
+# 위치 확인
+cv2.circle(drawing_image, (middle_x_act, middle_y_act), 10, (0, 255, 0), 3) 
+cv2.circle(drawing_image, (left_x_act, left_y_act), 10, (255, 255, 255), 3) 
+cv2.circle(drawing_image, (right_x_act, right_y_act), 10, (0, 0, 255), 3) 
+cv2.circle(drawing_image, (bottom_x_act, bottom_y_act), 10, (255, 0, 0), 3) 
 cv2.imwrite("stamp_test.jpg", drawing_image)
 
 
 
+# stamp resize
+target_length = right_x_act - left_x_act
+if (middle_y_act - target_length) < 0:
+    stamp = cv2.resize(king_stamp, dsize = [target_length, middle_y_act])
+else:
+    stamp = cv2.resize(king_stamp, dsize= [target_length, target_length])
+
+stamp_y, stamp_x, _ = stamp.shape
+
+roi = image[middle_y_act-stamp_y: middle_y_act, middle_x_act - math.floor(stamp_x/2) : middle_x_act + math.ceil(stamp_x/2)]
+
+img2gray = cv2.cvtColor(stamp, cv2.COLOR_BGR2GRAY)
+_, mask = cv2.threshold(img2gray, 170, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+img_bg = cv2.bitwise_and(roi, roi, mask=mask)
+stamp_fg = cv2.bitwise_and(stamp, stamp, mask=mask_inv)
+dst = cv2.add(img_bg, stamp_fg)
+
+new_img = image.copy()
+
+new_img[middle_y_act-stamp_y: middle_y_act, middle_x_act - math.floor(stamp_x/2) : middle_x_act + math.ceil(stamp_x/2)] = dst
+
+cv2.imwrite('stamp_test2.jpg', new_img)
 
 
-king_stamp = cv2.imread('/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/king.jpeg')
-
-cv2.imshow('test',king_stamp)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
-cv2.destroyWindow('test')
 
 
+position_holder = {0: [347, 67, 307, 411, -15.07], 1: [487, 103, 433, 566, 9.0], 2: [210, 98, 180, 268, 20.14], 3: [101, 70, 51, 127, 18.03]}
+
+imgmapper = {
+    0:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/king.jpeg',
+    1:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/noble.jpeg',
+    2:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/commoner.jpeg',
+    3:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/slave.jpeg'
+}
+
+rank_dict = {0:1,1:3,2:0,3:2}
+
+new_img = image.copy()
+
+person_idx = 2
+
+for person_idx in range(0,4):
+    psh = position_holder[person_idx]
+    stamp = cv2.imread(imgmapper[rank_dict[person_idx]])
+    middle_x, middle_y, lx, rx, deg = psh
+    target_length = rx - lx 
+    ratio = target_length/stamp.shape[1]
+    target_y = int(ratio*stamp.shape[0])
+    if (middle_y - target_y) < 0:
+        stamp = cv2.resize(stamp, dsize = [target_length, middle_y])
+    else:
+        stamp = cv2.resize(stamp, dsize= [target_length, int(ratio*stamp.shape[0])])
+
+    (h, w) = stamp.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D((cX, cY), deg, 1.0)
+    stamp = cv2.warpAffine(stamp, M, (w, h), borderValue=(255,255,255))
+
+    stamp_y, stamp_x, _ = stamp.shape
+    
+    #roi = image[middle_y-stamp_y: middle_y, middle_x - math.floor(stamp_x/2) : middle_x + math.ceil(stamp_x/2)]
+    roi = new_img[middle_y-stamp_y: middle_y, lx : rx]
+
+    img2gray = cv2.cvtColor(stamp, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(img2gray, 250, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    img_bg = cv2.bitwise_and(roi, roi, mask=mask)
+    stamp_fg = cv2.bitwise_and(stamp, stamp, mask=mask_inv)
+    dst = cv2.add(img_bg, stamp_fg)
+
+    #new_img[middle_y-stamp_y: middle_y, middle_x - math.floor(stamp_x/2) : middle_x + math.ceil(stamp_x/2)] = dst
+    new_img[middle_y-stamp_y: middle_y, lx : rx] = dst
+
+cv2.imwrite('stamp_test2.jpg', new_img)
+
+
+
+cv2.imwrite('test_img.jpg', roi)
+
+### stamp rotation
+import math
+x1, y1 = middle_x_act, middle_y_act
+x2, y2 = bottom_x_act, bottom_y_act
+rad = math.atan2(x2-x1,y2-y1)
+deg = (rad*180)/math.pi
+
+
+# 이미지의 크기를 잡고 이미지의 중심을 계산합니다.
+(h, w) = stamp.shape[:2]
+(cX, cY) = (w // 2, h // 2)
+ 
+# 이미지의 중심을 중심으로 이미지를 45도 회전합니다.
+M = cv2.getRotationMatrix2D((cX, cY), deg, 1.0)
+rotated_45 = cv2.warpAffine(stamp, M, (w, h), borderValue=(255,255,255))
+
+
+white_img = np.full((stamp.shape[0], stamp.shape[1], 3), (255, 255, 255), dtype=np.uint8)
+
+bgr = rotated_45[:,:,0:3]
+test_mask = cv2.inRange(bgr, (0,0,0), (0,0,0))
+bgr_new = bgr.copy()
+bgr_new[test_mask != (0)] = (255,255,255)
+
+
+test = cv2.bitwise_and(rotated_45, white_img)
+
+cv2.imwrite('rotate_test.jpg', rotated_45) 
+
+
+
+
+
+
+
+
+raw_img = cv2.imread('/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/test_img/p179506928231510_590.jpg')
+
+test_img = raw_img[:,180:268]
+
+
+cv2.imwrite('test_img.jpg', test_img)
--- web_test/src/api/whostheking.py
+++ web_test/src/api/whostheking.py
@@ -10,44 +10,6 @@ import torch.nn.functional as F
 import mediapipe as mp
 import math
 
-# data parsing
-class data_parser :
-    def __init__(self):
-        self.mp_face_mesh = mp.solutions.face_mesh
-        self.face_mesh = self.mp_face_mesh.FaceMesh(refine_landmarks = True, static_image_mode = True, max_num_faces = 4)
-    
-    def __call__(self, img):
-        result = self.generate_face_mesh(img)
-        if result == '-1':
-            return '-1'
-        result = self.get_landmarks_by_face(result)
-        data_holder = [self.generate_matrix(result[key]) for key in result.keys()]
-        return np.expand_dims(np.array(data_holder), axis = 1)
-
-    def get_length(self, x,y,z):
-        return math.sqrt((x[1]-x[0])**2+(y[1]-y[0])**2+(z[1]-z[0])**2)
-
-    def generate_face_mesh(self, img):
-        mesh_data = self.face_mesh.process(img)
-        return -1 if mesh_data.multi_face_landmarks == None else mesh_data
-
-    def get_landmarks_by_face(self, mesh_data):
-        landmark_holder = dict()
-        for i, j in enumerate(mesh_data.multi_face_landmarks):
-            landmark_holder[str(i+1)] = j.landmark
-        return landmark_holder
-    
-    def generate_matrix(self, landmark_info):
-        cal_arr = list()
-        for i in range(len(landmark_info)):
-            one, temp_arr = landmark_info[i], list()
-            for j in range(len(landmark_info)):
-                two = landmark_info[j]
-                temp_arr.append(self.get_length([one.x, two.x], [one.y, two.y], [one.z, two.z]))
-            cal_arr.append(temp_arr)
-        return cal_arr
-    
-
 # whos the king model
 class wtk_model(nn.Module):
     def __init__(self):
@@ -74,10 +36,87 @@ class wtk_model(nn.Module):
         x = self.ffn_fin(x)
         return x 
 
-  
-class response_generator:
+class wtk_temp_model(nn.Module):
     def __init__(self):
-        self.model = wtk_model()
+        super().__init__()
+        self.ffn_1 = nn.Linear(478*478, 16)
+        self.ffn_2 = nn.Linear(16,4)
+
+    def forward(self, x):
+        x = torch.flatten(x,1)
+        x = F.relu(self.ffn_1(x))
+        x = self.ffn_2(x)
+        return x
+
+
+
+
+
+
+
+# data parsing
+class data_parser :
+    def __init__(self):
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(refine_landmarks = True, static_image_mode = True, max_num_faces = 4)
+    
+    def __call__(self, img):
+        result = self.generate_face_mesh(img)
+        if result == '-1':
+            return '-1', '-1'
+        position_holder = self.get_data_for_stamping(result, (img.shape[0], img.shape[1]))
+        result = self.get_landmarks_by_face(result)
+        data_holder = [self.generate_matrix(result[key]) for key in result.keys()]
+        return np.expand_dims(np.array(data_holder), axis = 1), position_holder
+
+    def get_length(self, x,y,z):
+        return math.sqrt((x[1]-x[0])**2+(y[1]-y[0])**2+(z[1]-z[0])**2)
+
+    def generate_face_mesh(self, img):
+        mesh_data = self.face_mesh.process(img)
+        return -1 if mesh_data.multi_face_landmarks == None else mesh_data
+
+    def get_landmarks_by_face(self, mesh_data):
+        landmark_holder = dict()
+        for i, j in enumerate(mesh_data.multi_face_landmarks):
+            landmark_holder[str(i+1)] = j.landmark
+        return landmark_holder
+    
+    def generate_matrix(self, landmark_info):
+        cal_arr = list()
+        for i in range(len(landmark_info)):
+            one, temp_arr = landmark_info[i], list()
+            for j in range(len(landmark_info)):
+                two = landmark_info[j]
+                temp_arr.append(self.get_length([one.x, two.x], [one.y, two.y], [one.z, two.z]))
+            cal_arr.append(temp_arr)
+        return cal_arr
+    
+    def get_data_for_stamping(self,mesh_data, act_img_shape):
+        position_holder = dict()
+        act_y, act_x =  act_img_shape
+        for person_idx in range(0,4):
+            middle_x = mesh_data.multi_face_landmarks[person_idx].landmark[151].x
+            middle_y = mesh_data.multi_face_landmarks[person_idx].landmark[151].y
+            left_x = mesh_data.multi_face_landmarks[person_idx].landmark[162].x
+            right_x = mesh_data.multi_face_landmarks[person_idx].landmark[389].x
+            bottom_x = mesh_data.multi_face_landmarks[person_idx].landmark[199].x
+            bottom_y = mesh_data.multi_face_landmarks[person_idx].landmark[199].y
+            middle_x, middle_y, left_x, right_x, bottom_x, bottom_y = int(middle_x * act_x), int(middle_y * act_y), math.floor(left_x * act_x), math.ceil(right_x * act_x), int(bottom_x * act_x), int(bottom_y * act_y)
+            deg = (math.atan2(bottom_x - middle_x, bottom_y - middle_y)*180)/math.pi
+            position_holder[person_idx] = [middle_x, middle_y, left_x, right_x, round(deg,2)]
+        return position_holder
+
+
+
+
+
+
+  
+class proba_generator:
+    def __init__(self):
+        # self.model = wtk_model()
+        self.model = wtk_temp_model()
     
     def predict(self, single_data):
         single_data = np.expand_dims(single_data, axis = 0)
@@ -93,46 +132,101 @@ class response_generator:
         return proba_holder
 
 
+
+
+
+
+
+
+
+class response_generator:
+    def __init__(self, original_img):
+        self.img = original_img
+        self.imgmapper = { 
+            0:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/king.jpeg',
+            1:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/noble.jpeg' ,
+            2:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/commoner.jpeg' ,
+            3:'/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/stamps/slave.jpeg'
+            }
+
+    def rank_sorter(self, proba_list):
+        rank_dict = {0:None,1:None,2:None,3:None}
+        for label_num in range(0,4):    
+            ordered_list = [proba_list[person][0][label_num] for person in range(len(proba_list))]
+            temp_list = sorted(ordered_list)
+            flag = True
+            while flag:
+                cur_high = temp_list.pop()
+                cur_person = ordered_list.index(cur_high)
+                if rank_dict[cur_person] == None:
+                    rank_dict[cur_person] = label_num
+                    flag = False
+        return rank_dict
+
+    def stamp_img(self, rank_dict, position_info):
+        for person_idx in rank_dict.keys():
+            psh = position_info[person_idx]
+            stamp = cv2.imread(self.imgmapper[rank_dict[person_idx]])
+            middle_x, middle_y, target_length = psh
+            ratio = target_length/stamp.shape[1]
+            target_y = int(ratio*stamp.shape[0])
+            if (middle_y - target_y) < 0:
+                stamp = cv2.resize(stamp, dsize = [target_length, middle_y])
+            else:
+                stamp = cv2.resize(stamp, dsize= [target_length, int(ratio*stamp.shape[0])])
+            stamp_y, stamp_x, _ = stamp.shape
+            roi = self.img[middle_y-stamp_y: middle_y, middle_x - math.floor(stamp_x/2) : middle_x + math.ceil(stamp_x/2)]
+            img2gray = cv2.cvtColor(stamp, cv2.COLOR_BGR2GRAY)
+            _, mask = cv2.threshold(img2gray, 250, 255, cv2.THRESH_BINARY)
+            mask_inv = cv2.bitwise_not(mask)
+            img_bg = cv2.bitwise_and(roi, roi, mask=mask)
+            stamp_fg = cv2.bitwise_and(stamp, stamp, mask=mask_inv)
+            dst = cv2.add(img_bg, stamp_fg)
+            self.img[middle_y-stamp_y: middle_y, middle_x - math.floor(stamp_x/2) : middle_x + math.ceil(stamp_x/2)] = dst
+        return self.img
+
+
+
+
+
+
+
+
 # test run
 test_img = cv2.imread('/Users/hyunjun_bruce_lee/Documents/GIT/TenPJ_mk2/test_img/p179506928231510_590.jpg')
 
 test_obj = data_parser()
 
-resp_gen = response_generator()
 
-test_temp = test_obj(test_img)
+test_out = test_obj.generate_face_mesh(test_img)
 
-proba_list = resp_gen.predict_all(test_temp)
+x = test_out.multi_face_landmarks[2].landmark[162].x
+x
 
-for i in range(len(proba_list)):
-    print(proba_list[i])
+x*600
 
-rank_dict = {1:0,2:0,3:0,4:0}
 
-for label_num in range(0,4):    
-    temp_list = [proba_list[person][0][label_num] for person in range(len(proba_list))]
-    highest_person = temp_list.index(max(temp_list))
-    rank_dict[highest_person+1] = label_num
+proba_gen = proba_generator()
 
-rank_dict
+response_gen = response_generator(test_img)
 
-class response_generator:
-    def __init__(self, original_img):
-        self.img = original_img
-        self.imgmapper = {'label' : 'label_img_dir'}
-        pass
+test_temp, position_holder = test_obj(test_img)
 
-    def rank_sorter(self, probal_list):
-        rank_dict = {1:0,2:0,3:0,4:0}
-        for label_num in range(0,4):
-            temp_list = [proba_list[person][0][label_num] for person in range(len(proba_list))]
-            highest_person = temp_list.index(max(temp_list))
-            rank_dict[highest_person+1] = label_num
-        return rank_dict
+mx, my, lx, rx, deg = position_holder[3]
 
-    def stamp_img(self, rank_dict, position_info):
-        # position_info as dict type that holds position for head of each person 
-        pass
+
+
+proba_list = proba_gen.predict_all(test_temp)
+
+rank_dict = response_gen.rank_sorter(proba_list)
+
+out_img = response_gen.stamp_img(rank_dict, position_holder)
+
+cv2.imwrite('stamp_test3.jpg', out_img)
+
+
+
+
 
 
 # API
